f6b73ac73cef4ddb51b2b6acc653411fc905282c
Kasib00786 <kasib7890@gmail.com>
2026-04-15 13:24:55 +0530

    updated one

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: String,
+  phone: String,
+  gender: String,
+  dob: String,
+  bio: String,
+});
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+module.exports = mongoose.model("User", userSchema);
