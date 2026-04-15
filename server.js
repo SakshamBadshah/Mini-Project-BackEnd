2ee97afc37a3b87eabb043ff0f97432ec07e25ff
Kasib00786 <kasib7890@gmail.com>
2026-04-15 13:46:13 +0530

    done

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use("/api/addresses", addressRoutes);
 
 app.use("/api/cart", require("./routes/cartRoutes"));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
