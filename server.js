ef59ab266fc35a96fbf46b7cab88565b37541f4d
SakshamBadshah <sakshamchaturvedi911@gmail.com>
2026-04-15 16:15:36 +0530

    21

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
 
 const cors = require("cors");
 
@@ -10,9 +9,14 @@ const corsOptions = {
     "http://localhost:5173"
   ],
   credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
+app.use(express.json());
+
 const authRoutes = require("./routes/authRoutes");
 const connectDB = require("./config/db");
 require("dotenv").config();
