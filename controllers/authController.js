d8d3857d4bce3e87dbc6c1fdf596cd902c57dfb1
Kasib00786 <kasib7890@gmail.com>
2026-04-15 15:26:09 +0530

    10

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,17 +11,16 @@ exports.registerUser = async (req, res) => {
     return res.status(400).json({ message: "User already exists" });
   }
 
-  const hashedPassword = await bcrypt.hash(password, 10);
-
   const user = await User.create({
     name,
     email,
-    password: hashedPassword,
+    password,
   });
 
   res.json({
     _id: user._id,
     token: generateToken(user._id),
+    user: { name: user.name, email: user.email } 
   });
 };
 
